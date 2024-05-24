7b0af247f4a09af8149467b2c1d674f7fec2f0e7
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-05-24 20:09:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
+import os
 from flask import Flask, request
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import os
+import time
 
 app = Flask(__name__)
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = '/app/.chrome-for-testing/chrome-linux64/chrome'
 
 @app.route('/')
 def open_link():
@@ -19,16 +17,22 @@ def open_link():
     if not link:
         return "No link provided", 400
     
-    # Initialize the Chrome WebDriver using the provided binary location
-    chrome_driver_path = ChromeDriverManager().install()
-    driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
+    # Specify Chrome options
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = '/app/.chrome-for-testing/chrome-linux64/chrome'
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize the Chrome WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
 
     try:
         # Navigate to the specified URL
         driver.get(link)
-
+        
         # Wait for a few seconds to allow the page to load
-        driver.implicitly_wait(3)  # Adjust this time as needed
+        time.sleep(5)  # Adjust this time as needed
         
         # Get the current URL after the page has loaded
         current_url = driver.current_url
@@ -36,10 +40,10 @@ def open_link():
     finally:
         # Close the browser
         driver.quit()
-        # Remove ChromeDriver log files to avoid cluttering Heroku's filesystem
-        os.remove(chrome_driver_path)
 
     return f"Automation done. Current URL: {current_url}", 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    # Use the dynamic port assigned by Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
