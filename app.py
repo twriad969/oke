f84e197ce510a7f1ed40919d91aa135886aa4bd4
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-05-24 16:53:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 import time
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = '/app/.chrome-for-testing/chrome-linux64/chrome'
+# Function to create Chrome WebDriver with options
+def create_webdriver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = '/app/.chrome-for-testing/chrome-linux64/chrome'
+    return webdriver.Chrome(options=chrome_options)
 
 @app.route('/')
 def extract_id_and_generate_response():
@@ -20,26 +27,26 @@ def extract_id_and_generate_response():
 
     unique_id = link.split('/')[-1][1:]
     new_url = f"https://core.mdiskplay.com/box/terabox/video/{unique_id}.m3u8"
+    
+    try:
+        driver = create_webdriver()
+        driver.get(new_url)
+        time.sleep(5)
 
-    with webdriver.Chrome(options=chrome_options) as driver:
-        try:
-            driver.get(new_url)
-            # Wait for the page to fully load (you can adjust the wait time as needed)
-            time.sleep(5)
-
-            # Check if the page is fully loaded
-            if "404 Not Found" not in driver.title:
-                response = {'response': new_url}
-            else:
-                response = {'error': 'Failed to load content'}
-
-        except Exception as e:
-            # Handle any exceptions here
-            response = {'error': str(e)}
+        if "404 Not Found" not in driver.title:
+            response = {'response': new_url}
+        else:
+            response = {'error': 'Failed to load content'}
+        
+        logging.info(f"Processed link: {link}, response: {response}")
+    except Exception as e:
+        logging.error(f"Error processing link: {link}, error: {str(e)}")
+        response = {'error': str(e)}
+    finally:
+        driver.quit()
 
     return jsonify(response)
 
 if __name__ == '__main__':
-    # Running the app on the specified port
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port) 
