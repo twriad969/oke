1bd4b8b81c2372db0f29c6be84718ccb81897c42
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-05-24 20:33:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,6 @@ def open_url():
         return "No URL provided!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Heroku provides PORT dynamically
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
